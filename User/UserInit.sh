9e8c9bbe0d163ca06ad45f18fb64fe938549bade
ZeSantos0 <ze.santos0090@gmail.com>
2023-03-09 21:28:59 +0000

    user script

---
+++ User/UserInit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+initialPort=11000
+count=1
+> ../Common/Uconfiguration.txt
+
+while [ $count -le $1 ]; do
+  echo "$((count-1)) localhost $initialPort" >> ../Common/Uconfiguration.txt
+  initialPort=$((initialPort+1))
+  count=$((count+1))
+done
+
+count=1
+while [ $count -le $1 ]; do
+  id=$(printf "%d" $((count-1)))
+  gnome-terminal \
+  -e "bash -c 'mvn exec:java -Dexec.args="$id"; bash'"
+  >../Common/resources/U"$id"public.key
+   >resources/U"$id"private.key
+  count=$((count+1))
+done
