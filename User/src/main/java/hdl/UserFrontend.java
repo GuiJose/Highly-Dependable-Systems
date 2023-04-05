2cef0060c0fd62fb15bf95c63fd9cbf6228a8d1d
GuiJose <guilherme.silva.santos@tecnico.ulisboa.pt>
2023-04-05 19:22:42 +0100

    Strong reads fix

--- User/src/main/java/hdl/UserFrontend.java
+++ User/src/main/java/hdl/UserFrontend.java
@@ -135,7 +135,7 @@ public class UserFrontend {
                 int mostCommonTimestamp = mostCommonTimestamp(M);
                 int mostCommonBalance = mostCommonBalance(M, mostCommonTimestamp);
                 this.readTimeStamp = mostCommonBalance;
-                System.out.println("Your balance is: " + mostCommonBalance);                
+                System.out.println("Your balance is: " + mostCommonBalance + " with timestamp:" + this.readTimeStamp);                
             }
         }
     }
